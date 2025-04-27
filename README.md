7653fca9099d2f6a1414e506414cb686399c1155
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2025-04-27 17:58:23 +0300

    finished but need to fix some bugs

---
+++ README.md
@@ -0,0 +1,43 @@
+# Whack-a-Mole
+
+A simple Whack-a-Mole game built using JavaScript, HTML, and CSS.
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Overview
+
+Whack-a-Mole is a fun and interactive browser game where players try to "whack" moles that pop up randomly on the screen. The goal is to score as many points as possible within a given time.
+
+## Features
+
+- Randomly appearing moles
+- Score tracking
+- Timer functionality
+- Responsive design
+
+## Installation
+
+1. Clone the repository:
+  ```bash
+  git clone https://github.com/your-username/whack-a-mole-js.git
+  ```
+2. Navigate to the project directory:
+  ```bash
+  cd whack-a-mole-js
+  ```
+3. Open `index.html` in your browser to play the game.
+
+## Usage
+
+- Click on the moles as they appear to score points.
+- The game ends if you click piranha plant :).
+- Try to beat your high score!
+
+
--- mole.js
+++ mole.js
@@ -1,8 +1,27 @@
 let currMoleTile;
 let currPlantTile;
+let score = 0;
+let gameOver = false;
+let moleInterval = 1000; // 1 second
+let plantInterval = 2000; // 0.7 seconds
 
 window.onload = function(){
   setGame();
+}
+
+function intervalDecrease(){
+  if(moleInterval > 600){
+    if(score>50){
+      moleInterval-=500;
+      console.log("Mole Interval: "+moleInterval.toString());
+    }
+    }
+  if(plantInterval > 600){
+    if(score>50){
+      plantInterval-=500;
+      console.log("Plant Interval: "+plantInterval.toString());
+    }
+  }
 }
 
 function setGame(){
@@ -12,16 +31,21 @@ for(let i=0; i<9; i++){
 
   let tile = document.createElement("div");
   tile.id = i.toString();
+  tile.addEventListener("click", selectTile);
   document.getElementById("board").appendChild(tile);
 }
 
-setInterval(setMole, 2000);
+setInterval(setMole, moleInterval);
+setInterval(setPlant, plantInterval);
+
   
 }
 
 
 function setMole(){
-
+  if(gameOver){
+    return;
+  }
   if(currMoleTile != null){
     currMoleTile.removeChild(currMoleTile.firstChild);
   }
@@ -30,6 +54,9 @@ function setMole(){
   mole.src = "./monty-mole.png";
 
   let num = getRandomTile();
+  if(currPlantTile && num == currPlantTile.id){
+    num = getRandomTile();
+  }
   currMoleTile = document.getElementById(num);
   currMoleTile.appendChild(mole);
 }
@@ -40,9 +67,39 @@ function getRandomTile(){
 }
 
 function setPlant(){
+  if(gameOver){
+    return;
+  }
 if(currPlantTile != null){
     currPlantTile.removeChild(currPlantTile.firstChild);
   }
   let plant = document.createElement("img");
   plant.src = "./piranha-plant.png";
+  let num = getRandomTile();
+  if(currMoleTile && num == currMoleTile.id){
+    num = getRandomTile();
+  }
+  // make sure the plant and mole are not in the same tile
+// if they are, get a new random tile for the plant
+  // if they are not, set the plant in the tile
+  currPlantTile = document.getElementById(num);
+  currPlantTile.appendChild(plant);
+}
+
+function selectTile(){
+  intervalDecrease();
+  
+  if(this === currMoleTile){
+    score += 10;
+    document.getElementById("score").innerText = score.toString();
+  }
+  if(this === currPlantTile){
+    score -= 10;
+    document.getElementById("score").innerText = "Game Over Hal "+score.toString();
+    gameOver = true;
+    if(gameOver){
+      alert("Game Over Hal Your Score: "+score.toString());
+      return;
+    }
+  }
 }
